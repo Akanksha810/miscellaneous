76829dc01565604ad2950146aba26221a09a6876
biswajitghosh <biswajitghosh@localhost>
2022-02-27 13:53:29 +0530

    Modified Stream method

--- src/main/java/com/demo/utils/AlbumUtil.java
+++ src/main/java/com/demo/utils/AlbumUtil.java
@@ -9,60 +9,61 @@ import java.util.stream.Collectors;
 import com.demo.model.Album;
 
 public class AlbumUtil {
-	
+
 	private static List<Album> list = new ArrayList<>();
 	static {
-		
-		list.add(new Album("Artist1","Album1"));
-		list.add(new Album("Artist2","Album1"));
-		list.add(new Album("Artist3","Album1"));
-		list.add(new Album("Artist2","Album2"));
-		list.add(new Album("Artist3","Album2"));
-		list.add(new Album("Artist3","Album3"));
-		list.add(new Album("Artist1","Album2"));
-		list.add(new Album("Artist3","Album4"));
-		list.add(new Album("Artist1","Album3"));
+
+		list.add(new Album("Artist1", "Album1"));
+		list.add(new Album("Artist2", "Album1"));
+		list.add(new Album("Artist3", "Album1"));
+		list.add(new Album("Artist2", "Album2"));
+		list.add(new Album("Artist3", "Album2"));
+		list.add(new Album("Artist3", "Album3"));
+		list.add(new Album("Artist1", "Album2"));
+		list.add(new Album("Artist3", "Album4"));
+		list.add(new Album("Artist1", "Album3"));
 	}
-	
-	public static Map<String, List<String>> getAlbumsPerArtist()  {
-		
+
+	public static Map<String, List<String>> getAlbumsPerArtist() {
+
 		Map<String, List<String>> albumMap = new HashMap<>();
-		
-		for(Album album : list) {
-			
-			String key = album.getArtist();		
-			
-			if(albumMap.containsKey(key)) {
-				
+
+		for (Album album : list) {
+
+			String key = album.getArtist();
+
+			if (albumMap.containsKey(key)) {
+
 				List<String> albums = albumMap.get(key);
 				albums.add(album.getAlbum());
 				albumMap.put(key, albums);
 			}
-			
-			else {	
+
+			else {
 				List<String> albums = new ArrayList<>();
 				albums.add(album.getAlbum());
 				albumMap.put(key, albums);
 			}
-		
+
 		}
-		
+
 		return albumMap;
 	}
-	
-	public static Map<String, List<Album>> getAblumListByArtist() {
-		
-		return list.stream().collect(Collectors.groupingBy(Album::getArtist));
+
+	public static Map<String, List<String>> getAblumListByArtist() {
+
+		return list.stream().collect(
+				Collectors.groupingBy(Album::getArtist, Collectors.mapping(Album::getAlbum, Collectors.toList())));
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		Map<String, List<String>> albumMap = getAlbumsPerArtist();
 		for (Map.Entry<String, List<String>> entry : albumMap.entrySet()) {
 			System.out.println(entry.getKey() + " : " + entry.getValue().size());
 			System.out.println("Albums : " + entry.getValue());
 		}
-		
-		System.out.println("Using stream: "+getAblumListByArtist() );
+
+		System.out.println("Using stream: " + getAblumListByArtist());
 	}
 }
